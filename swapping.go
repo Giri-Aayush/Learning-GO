51ed4076259eef3fa8f135c26b15244eb83917ce
Giri-Aayush <aayushgiri1234@gmail.com>
2023-08-14 20:41:16 +0530

    swapping numbers in slice without return

--- swapping.go
+++ swapping.go
@@ -2,14 +2,17 @@ package main
 
 import "fmt"
 
-func swappingSliceElements(v []int) []int {
+func swappingSliceElements(v []int) {
 	for i, j := 0, len(v)-1; i < j; i, j = i+1, j-1 {
 		v[i], v[j] = v[j], v[i]
 	}
-	return v
 }
+
+// No need of return since slices are passed by reference
 
 func main() {
 	testSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(swappingSliceElements(testSlice))
+	swappingSliceElements(testSlice)
+	fmt.Println(testSlice)
+	//the original slice values change
 }
